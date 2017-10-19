ecc0439f800b5935df0dda76e493a12438e030f9
Ras <g.vandoros1991@gmail.com>
2017-10-19 23:16:52 +0100

    Database

--- WindowsFormsApplication2/UIManager.cs
+++ WindowsFormsApplication2/UIManager.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApplication2
 {
---
+++ WindowsFormsApplication2/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class User
+    {
+        enum Job
+        {
+            Receptionist,
+            Manager
+        };
+
+        private string password;
+        private string username;
+        private Job job;
+        
+        public User(string password, string username)
+        {
+
+        }
+    }
+}
